bba7b7cb6b5aa29833006ac65a9acfe91d9c4242
Olaf Veerman <olaf@developmentseed.org>
2017-10-25 20:58:11 -0400

    Update production.js

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -4,7 +4,7 @@
  */
 module.exports = {
   environment: 'production',
-  api: 'http://api.ruralaccess.info',
+  api: 'http://34.204.232.68',
   iDEditor: 'http://id.ruralaccess.info',
   mbtoken: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q'
 };
